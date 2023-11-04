83330fe7e6b94ecece69d0f09f541c454645badd
Alec Patterson <8888alec@gmail.com>
2023-11-04 18:09:07 -0400

    Restyled home page

--- client/src/pages/homePage.vue
+++ client/src/pages/homePage.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- class="bg-image" -->
   <q-page padding class="bg-image">
-    <q-btn class="glossy q-my-sm" color="positive" label="Start Here" to="queryOne"/>
+    <q-btn class="glossy q-my-sm" color="primary" label="Start Here" to="queryOne"/>
 
     <div class="row q-gutter-md">
 
-      <q-card class="img-and-text-card">
-        <q-img src="~/assets/DALL-E-2023-baseballstatschart.png">
+      <q-card class="queryCards">
+        <q-img fit="cover" src="~/assets/q1-cardImage.png" class="query-img-card">
           <div class="absolute-bottom text-h6">
             {{ q1_title }}
           </div>
@@ -17,8 +17,8 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="img-and-text-card">
-        <q-img src="~/assets/DALL-E-2023-baseballstatschart.png">
+      <q-card fit="contain" class="queryCards" >
+        <q-img src="~/assets/q2-cardImage.png" class="query-img-card">
           <div class="absolute-bottom text-h6">
             {{ q2_title }}
           </div>
@@ -29,8 +29,8 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="img-and-text-card">
-        <q-img src="~/assets/DALL-E-2023-baseballstatschart.png">
+      <q-card class="queryCards">
+        <q-img src="~/assets/q3-cardImage1.png" class="query-img-card">
           <div class="absolute-bottom text-h6">
             {{ q3_title }}
           </div>
@@ -41,8 +41,8 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="img-and-text-card">
-        <q-img src="~/assets/DALL-E-2023-baseballstatschart.png">
+      <q-card class="queryCards">
+        <q-img src="~/assets/q4-cardImage.png" class="query-img-card">
           <div class="absolute-bottom text-h6">
             {{ q4_title }}
           </div>
@@ -53,8 +53,8 @@
         </q-card-section>
       </q-card>
 
-      <q-card class="img-and-text-card">
-        <q-img src="~/assets/DALL-E-2023-baseballstatschart.png">
+      <q-card class="queryCards">
+        <q-img src="~/assets/q5-cardImage.png" class="query-img-card">
           <div class="absolute-bottom text-h6">
             {{ q5_title }}
           </div>
@@ -81,7 +81,7 @@ export default defineComponent({
         q1_description: 'The connection between runs-to-outs by salary range, which helps explore whether higher paid players tend to have better offensive performance and how this relationship evolves over different seasons',
         q2_title: 'Team Spending Per Win',
         q2_description: 'Team spending and winning percentage, which sheds light on whether higher team spending translates into more victories',
-        q3_title: 'Foreign-born MLB Players',
+        q3_title: 'Foreign-born Wins/Losses',
         q3_description: 'The distribution of foreign-born MLB players and their relation to team wins/losses, which aims to understand the contributions of increasingly global players',
         q4_title: 'Player Hieght and Performance',
         q4_description: 'The influence of player height on performance, which addresses questions about whether taller or shorter players have advantages in specific aspects of the game',
@@ -96,22 +96,21 @@ export default defineComponent({
 })
 </script>
 
-<style>
+<style >
   .bg-image {
     background-image: url('C:/dev/dbProject/client/src/assets/bg4.png');
-    background-size: contain;
-    background-repeat: repeat;
+    background-size: fit;
   }
   .text.color {
     color: black;
   }
-  .query-cards {
-    width: 100%;
-    max-width: 250px;
-  }
-  .img-and-text-card
+  .queryCards
   {
     width: 100%;
     max-width: 350px;
+    
   }
+  /* .query-img-card {
+    height: 200px;
+  } */
 </style>
