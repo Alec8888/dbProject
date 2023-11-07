11e078c3ea256927f8fc3bebccd8e5e17684a354
Alec Patterson <8888alec@gmail.com>
2023-11-06 20:34:43 -0500

    Added variables to q2 sql and formatted

--- Queries/query2.sql
+++ Queries/query2.sql
@@ -1,27 +1,15 @@
-WITH 
-    teamwinsperyear AS (
-        SELECT 
-            year, 
-            wins
-        FROM "JOSEPH.BARRON".teamparticipatesseason teamparticipatesseason
-        WHERE
-            teamparticipatesseason.team_id = 'ATL' AND
-            teamparticipatesseason.year BETWEEN 1985 AND 2020
-    ),        
-    teamsalariesperyear AS (
-        SELECT 
-            year,
-            SUM(playsforateam.salary) AS total_spend
-        FROM "JOSEPH.BARRON".playsforateam playsforateam
-        WHERE 
-            playsforateam.team_id = 'ATL' AND
-            playsforateam.year BETWEEN 1985 AND 2020
-        GROUP BY year
-
-    )
-    SELECT 
-        teamsalariesperyear.year AS year,
-        ROUND(teamsalariesperyear.total_spend / teamwinsperyear.wins, 2) AS spend_per_win
-    FROM teamsalariesperyear
-    JOIN teamwinsperyear ON teamwinsperyear.year = teamsalariesperyear.year
-    ORDER BY year
+WITH teamwinsperyear AS (
+    SELECT year, wins
+    FROM teamparticipatesseason
+    WHERE teamparticipatesseason.team_id = :team AND teamparticipatesseason.year BETWEEN :startYear AND :endYear
+),        
+teamsalariesperyear AS (
+    SELECT year, SUM(playsforateam.salary) AS total_spend
+    FROM playsforateam
+    WHERE playsforateam.team_id = :team AND playsforateam.year BETWEEN :startYear AND :endYear
+    GROUP BY year
+)
+SELECT teamsalariesperyear.year AS year, ROUND(teamsalariesperyear.total_spend / teamwinsperyear.wins, 2) AS spend_per_win
+FROM teamsalariesperyear
+JOIN teamwinsperyear ON teamwinsperyear.year = teamsalariesperyear.year
+ORDER BY year
