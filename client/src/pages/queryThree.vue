356376e21b417c668510fbadcfee2091bdbe6d31
joe <67911762+saberzap@users.noreply.github.com>
2023-11-12 10:22:28 -0500

    Prefix 'marker' prop with semicolon in remaining pages, format files

--- client/src/pages/queryThree.vue
+++ client/src/pages/queryThree.vue
@@ -1,35 +1,22 @@
 <template>
-  
   <q-page padding>
     <div class="q-gutter-md">
       <q-card>
         <q-card-section>
-          <div class="text-h6">
-            Sub title
-          </div>
-          <div class="text-body1">
-            text body...  
-          </div>
+          <div class="text-h6">Sub title</div>
+          <div class="text-body1">text body...</div>
         </q-card-section>
       </q-card>
       <q-card>
         <q-card-section>
-          <div class="text-h6">
-            Methodology
-          </div>
-          <div class="text-body1">
-            text body...
-          </div>
+          <div class="text-h6">Methodology</div>
+          <div class="text-body1">text body...</div>
         </q-card-section>
       </q-card>
       <q-card>
         <q-card-section>
-          <div class="text-h6">
-            Analysis and Visualization
-          </div>
-          <div class="text-body1">
-            Text body..  
-          </div>
+          <div class="text-h6">Analysis and Visualization</div>
+          <div class="text-body1">Text body..</div>
         </q-card-section>
       </q-card>
       <q-card>
@@ -39,7 +26,7 @@
           </div>
         </q-card-section>
         <q-card-section>
-            <q-range
+          <q-range
             name="year_range"
             v-model="year_range"
             :min="1871"
@@ -47,14 +34,14 @@
             :step="1"
             label-always
             color="primary"
-            markers=true
+            :markers="true"
           />
         </q-card-section>
         <q-card-section>
-          <q-btn 
+          <q-btn
             class="glossy"
-            :loading="progress" 
-            color="accent" 
+            :loading="progress"
+            color="accent"
             @click="runQuery3"
           >
             Run Visualization
@@ -63,23 +50,27 @@
               Calculating...
             </template>
           </q-btn>
-          <q-btn 
+          <q-btn
             class="glossy q-ml-sm"
-            :loading="progress" 
-            color="primary" 
+            :loading="progress"
+            color="primary"
             @click="reset"
             label="Reset"
           />
         </q-card-section>
       </q-card>
       <q-card>
-        
-          <q-img v-if="showPlaceholder" fit="fill" src="~/assets/q3-cardImage.png" class="query-img-card"/>
-        
+        <q-img
+          v-if="showPlaceholder"
+          fit="fill"
+          src="~/assets/q3-cardImage.png"
+          class="query-img-card"
+        />
+
         <q-card-section v-if="showVisualization">
           <div>
-          <!--this list is for the demo sql query-->
-          <q-list bordered separator>
+            <!--this list is for the demo sql query-->
+            <q-list bordered separator>
               <q-item v-for="item in dataFromOracle" :key="item.id">
                 <q-item-section>{{ item[0] }}</q-item-section>
                 <q-item-section>{{ item[1] }}</q-item-section>
@@ -88,24 +79,23 @@
             </q-list>
           </div>
         </q-card-section>
-
       </q-card>
       <div class="q-gutter-md flex justify-center q-mr-lg">
         <q-btn
           class="glossy"
           rounded
           size="lg"
-          color="primary" 
+          color="primary"
           icon="home"
-          to="/" 
+          to="/"
         />
         <q-btn
-          class="glossy" 
+          class="glossy"
           rounded
           size="lg"
           color="primary"
           icon="navigate_before"
-          to="/queryTwo" 
+          to="/queryTwo"
         />
         <q-btn
           class="glossy"
@@ -113,9 +103,8 @@
           size="lg"
           color="accent"
           icon-right="navigate_next"
-          to="queryFour" 
+          to="queryFour"
         />
-
       </div>
     </div>
   </q-page>
@@ -123,25 +112,27 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       dataFromOracle: [],
-      cityName: 'Tampa',
-      country: 'USA',
+      cityName: "Tampa",
+      country: "USA",
       progress: false,
       year_range: {
         min: 1871,
-        max: 2022
+        max: 2022,
       },
       showPlaceholder: true,
-      showVisualization: false
-    }
+      showVisualization: false,
+    };
   },
   methods: {
-    async runQuery3 () {
+    async runQuery3() {
       this.progress = true;
-      
-      let response = await fetch(`http://localhost:4000/api?name_from_client=${this.cityName}&country_from_client=${this.country}`);
+
+      let response = await fetch(
+        `http://localhost:4000/api?name_from_client=${this.cityName}&country_from_client=${this.country}`
+      );
       let data = await response.json();
       this.dataFromOracle = data;
       console.log(data);
@@ -149,15 +140,14 @@ export default {
       this.progress = false;
       this.showPlaceholder = false;
       this.showVisualization = true;
-
     },
-    reset () {
+    reset() {
       this.dataFromOracle = [];
       this.showPlaceholder = true;
       this.showVisualization = false;
       this.year_range.min = 1871;
       this.year_range.max = 2022;
-    }
-  }
-}
+    },
+  },
+};
 </script>
