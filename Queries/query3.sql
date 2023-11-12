214e19effc14be2754ab530c1a785677f83a2829
joe <67911762+saberzap@users.noreply.github.com>
2023-11-12 12:00:39 -0500

    Add sql file for query3 and populate with limited version of query3

---
+++ Queries/query3.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+    PlaysForATeam.year, 
+    ROUND(SUM(CASE WHEN Player.birth_country != 'USA' THEN 1 ELSE 0 END) OVER(PARTITION BY PlaysForATeam.team_id, PlaysForATeam.year) / 
+           (SUM(CASE WHEN Player.birth_country != 'USA' THEN 1 ELSE 0 END) OVER(PARTITION BY PlaysForATeam.team_id, PlaysForATeam.year) 
+          + SUM(CASE WHEN Player.birth_country = 'USA' THEN 1 ELSE 0 END) OVER(PARTITION BY PlaysForATeam.team_id, PlaysForATeam.year)) * 100, 2) foreign_born_team_percentage
+FROM PlaysForATeam
+LEFT JOIN Player
+    ON PlaysForATeam.player_id = Player.player_id 
+LEFT JOIN TeamParticipatesSeason
+    ON PlaysForATeam.team_id = TeamParticipatesSeason.team_id AND PlaysForATeam.year = TeamParticipatesSeason.year
+WHERE PlaysForATeam.team_id = :team AND PlaysForATeam.year BETWEEN :startYear AND :endYear
+ORDER BY PlaysForATeam.year
