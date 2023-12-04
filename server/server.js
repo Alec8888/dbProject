2a7b3d74f2d27a7fd146d889a5a4dac4fb91ccbf
Alec Patterson <8888alec@gmail.com>
2023-12-04 13:37:03 -0500

    Organized server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,14 @@ require("dotenv").config();
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
+// Oracle connection parameters
+const oracledb = require("oracledb");
+let connectionConfig = {
+  user: dbUser,
+  password: dbPassword,
+  connectString: "oracle.cise.ufl.edu:1521/orcl",
+};
+
 // EXPRESS
 const express = require("express");
 const app = express();
@@ -30,25 +38,47 @@ app.use(function (req, res, next) {
   next();
 });
 
-// connect to DB config
-const oracledb = require("oracledb");
-let connectionConfig = {
-  user: dbUser,
-  password: dbPassword,
-  connectString: "oracle.cise.ufl.edu:1521/orcl", // Typically in 'hostname:port/serviceName' format
-};
+const fs = require("fs");
 
-// Connection to Oracle DB
-async function run(name_submitted) {
+async function runQuery1(start, end) {
   let connection;
   let result;
   try {
     connection = await oracledb.getConnection(connectionConfig);
     console.log("Successfully connected to Oracle!");
-    result = await connection.execute(
-      "SELECT name, population FROM CITY WHERE name=:name",
-      { name: name_submitted }
-    );
+
+    let sql;
+    try {
+      sql = fs.readFileSync("../Queries/query1.sql").toString();
+    } catch (err) {
+      console.error('Error reading SQL file', err);
+    }
+
+    result = await connection.execute(sql, { startYear: start, endYear: end });
+    console.log(result.rows);
+  } catch (err) {
+    console.error("Error connecting to the database", err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error("Error closing the database connection", err);
+      }
+    }
+  }
+  return result.rows;
+};
+
+
+async function getTeamsInRange(start, end) {
+  let connection;
+  let result;
+  try {
+    connection = await oracledb.getConnection(connectionConfig);
+    console.log("Successfully connected to Oracle!");
+    const sql = fs.readFileSync("../Queries/getTeamsInRange.sql").toString();
+    result = await connection.execute(sql, { startYear: start, endYear: end });
     console.log(result.rows);
   } catch (err) {
     console.error("Error connecting to the database", err);
@@ -63,7 +93,7 @@ async function run(name_submitted) {
   }
   return result.rows;
 }
-const fs = require("fs");
+
 async function runQuery2(start, end, tID) {
   let connection;
   let result;
@@ -118,14 +148,104 @@ async function runQuery3(start, end, tID) {
   return result.rows;
 }
 
-async function getTeamsInRange(start, end) {
+async function runQuery4obp(start, end) {
   let connection;
   let result;
   try {
     connection = await oracledb.getConnection(connectionConfig);
     console.log("Successfully connected to Oracle!");
-    const sql = fs.readFileSync("../Queries/getTeamsInRange.sql").toString();
+
+    let sql;
+    try {
+      sql = fs.readFileSync("../Queries/query4obp.sql").toString();
+    } catch (err) {
+      console.error('Error reading SQL file', err);
+    }
+
     result = await connection.execute(sql, { startYear: start, endYear: end });
+    console.log(result.rows);
+  } catch (err) {
+    console.error("Error connecting to the database", err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error("Error closing the database connection", err);
+      }
+    }
+  }
+  return result.rows;
+}
+
+async function runQuery4obpa(start, end) {
+  let connection;
+  let result;
+  try {
+    connection = await oracledb.getConnection(connectionConfig);
+    console.log("Successfully connected to Oracle!");
+
+    let sql;
+    try {
+      sql = fs.readFileSync("../Queries/query4obpa.sql").toString();
+    } catch (err) {
+      console.error('Error reading SQL file', err);
+    }
+
+    result = await connection.execute(sql, { startYear: start, endYear: end });
+    console.log(result.rows);
+  } catch (err) {
+    console.error("Error connecting to the database", err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error("Error closing the database connection", err);
+      }
+    }
+  }
+  return result.rows;
+}
+
+async function runQuery4rf(start, end) {
+  let connection;
+  let result;
+  try {
+    connection = await oracledb.getConnection(connectionConfig);
+    console.log("Successfully connected to Oracle!");
+
+    let sql;
+    try {
+      sql = fs.readFileSync("../Queries/query4rf.sql").toString();
+    } catch (err) {
+      console.error('Error reading SQL file', err);
+    }
+
+    result = await connection.execute(sql, { startYear: start, endYear: end });
+    console.log(result.rows);
+  } catch (err) {
+    console.error("Error connecting to the database", err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error("Error closing the database connection", err);
+      }
+    }
+  }
+  return result.rows;
+}
+
+async function runQuery5(percentile, start, end) {
+  let connection;
+  let result;
+  try {
+    connection = await oracledb.getConnection(connectionConfig);
+    console.log("Successfully connected to Oracle!");
+    const sql = fs.readFileSync("../Queries/query5.sql").toString();
+    result = await connection.execute(sql, { percentile: percentile, startYear: start, endYear: end });
     console.log(result.rows);
   } catch (err) {
     console.error("Error connecting to the database", err);
@@ -184,147 +304,6 @@ app.post("/feedback", async (req, res) => {
   res.send("Feedback received");
 });
 
-async function runQuery5(percentile, start, end) {
-  let connection;
-  let result;
-  try {
-    connection = await oracledb.getConnection(connectionConfig);
-    console.log("Successfully connected to Oracle!");
-    const sql = fs.readFileSync("../Queries/query5.sql").toString();
-    result = await connection.execute(sql, { percentile: percentile, startYear: start, endYear: end });
-    console.log(result.rows);
-  } catch (err) {
-    console.error("Error connecting to the database", err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error("Error closing the database connection", err);
-      }
-    }
-  }
-  return result.rows;
-}
-
-async function runQuery4obp(start, end) {
-  let connection;
-  let result;
-  try {
-    connection = await oracledb.getConnection(connectionConfig);
-    console.log("Successfully connected to Oracle!");
-
-    let sql;
-    try {
-      sql = fs.readFileSync("../Queries/query4obp.sql").toString();
-    } catch (err) {
-      console.error('Error reading SQL file', err);
-    }
-
-    result = await connection.execute(sql, { startYear: start, endYear: end });
-    console.log(result.rows);
-  } catch (err) {
-    console.error("Error connecting to the database", err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error("Error closing the database connection", err);
-      }
-    }
-  }
-  return result.rows;
-}
-async function runQuery4obpa(start, end) {
-  let connection;
-  let result;
-  try {
-    connection = await oracledb.getConnection(connectionConfig);
-    console.log("Successfully connected to Oracle!");
-
-    let sql;
-    try {
-      sql = fs.readFileSync("../Queries/query4obpa.sql").toString();
-    } catch (err) {
-      console.error('Error reading SQL file', err);
-    }
-
-    result = await connection.execute(sql, { startYear: start, endYear: end });
-    console.log(result.rows);
-  } catch (err) {
-    console.error("Error connecting to the database", err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error("Error closing the database connection", err);
-      }
-    }
-  }
-  return result.rows;
-}
-async function runQuery4rf(start, end) {
-  let connection;
-  let result;
-  try {
-    connection = await oracledb.getConnection(connectionConfig);
-    console.log("Successfully connected to Oracle!");
-
-    let sql;
-    try {
-      sql = fs.readFileSync("../Queries/query4rf.sql").toString();
-    } catch (err) {
-      console.error('Error reading SQL file', err);
-    }
-
-    result = await connection.execute(sql, { startYear: start, endYear: end });
-    console.log(result.rows);
-  } catch (err) {
-    console.error("Error connecting to the database", err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error("Error closing the database connection", err);
-      }
-    }
-  }
-  return result.rows;
-}
-
-async function runQuery1(start, end) {
-  let connection;
-  let result;
-  try {
-    connection = await oracledb.getConnection(connectionConfig);
-    console.log("Successfully connected to Oracle!");
-
-    let sql;
-    try {
-      sql = fs.readFileSync("../Queries/query1.sql").toString();
-    } catch (err) {
-      console.error('Error reading SQL file', err);
-    }
-
-    result = await connection.execute(sql, { startYear: start, endYear: end });
-    console.log(result.rows);
-  } catch (err) {
-    console.error("Error connecting to the database", err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error("Error closing the database connection", err);
-      }
-    }
-  }
-  return result.rows;
-}
-
 async function countTuples() {
   let connection;
   let result;
@@ -358,11 +337,6 @@ async function countTuples() {
 app.use(express.static("../spa/"));
 const server = app.listen(4000, () => {
   console.log("server started at localhost:4000");
-});
-
-app.get("/api", async (req, res) => {
-  const rows = await run(req.query.name_from_client);
-  res.send(rows);
 });
 
 app.get("/q3/teams_in_range", async (req, res) => {
